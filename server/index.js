68d25287193cdf48cee9f36e0f4ccce4801e115a
Fernanda <mangorifernanda@gmail.com>
2023-06-01 10:57:15 +0700

    server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 import express from 'express';
-import bodyParser from 'bodyParser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://mangorifernanda:1234@datamemories.sd5icyt.mongodb.net/?retryWrites=true&w=majority'
+const PORT =  process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message))
+
+// mongoose.set('useFindAndModify', false)
