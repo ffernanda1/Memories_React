6af7e018cc002fb34bce4cc0672307f135684687
Fernanda <mangorifernanda@gmail.com>
2023-07-06 19:36:00 +0700

    auth progress error

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,6 +45,7 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
     const { id } = req.params;
+    console.log(id)
 
     try {
         if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with that id');
@@ -60,6 +61,7 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const { id } = req.params;
+    console.log(id)
     try {
         if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with that id');
 
