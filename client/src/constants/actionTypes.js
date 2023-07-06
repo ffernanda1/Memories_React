3e8299e8a0c1ce1845c8137f576e618127b26dff
Fernanda <mangorifernanda@gmail.com>
2023-07-06 19:35:02 +0700

    auth progress error

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -5,3 +5,8 @@ export const FETCH_ALL = 'FETCH_ALL';
 
 export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
+
+export const AUTHs = 'AUTH';
+export const LOGOUTs = 'LOGOUT';
+export const AUTHd = 'AUTH';
+export const LOGOUTd = 'LOGOUT';
